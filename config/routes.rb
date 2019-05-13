ca727c98ac917773990e5de372c908c1672ea2aa
Joshua Drumm <joshing_you1@yahoo.com>
2019-05-13 14:03:48 -0400

    Started working on login/logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'home#show'
   resources :messages
   resources :users
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
