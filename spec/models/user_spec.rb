5cff5befc303ebc1b9b9633645bddbffac27c00d
Joshua Drumm <joshing_you1@yahoo.com>
2019-07-20 00:19:01 -0400

    Added a few tests for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'ActiveRecord associations' do
+    it 'Should have many posts' do
+      expect(User.reflect_on_association(:posts).macro).to be(:has_many)
+    end
+
+    it 'Should have many comments' do
+      expect(User.reflect_on_association(:comments).macro).to be(:has_many)
+    end
+  end
+
+  context 'Validations' do
+    it 'Should error if email is not valid' do
+      new_user = User.new
+      new_user.first_name = 'john'
+      new_user.last_name = 'doe'
+      new_user.email = ''
+      new_user.password = 'test'
+      new_user.password_confirmation = 'test'
+      expect(new_user).to_not be_valid
+    end
+
+    it 'Should error if first_name is not valid' do
+      new_user = User.new
+      new_user.first_name = ''
+      new_user.last_name = 'doe'
+      new_user.email = 'john@mail.com'
+      new_user.password = 'test'
+      new_user.password_confirmation = 'test'
+      expect(new_user).to_not be_valid
+    end
+
+    it 'Should error if last_name is not valid' do
+      new_user = User.new
+      new_user.first_name = 'john'
+      new_user.last_name = ''
+      new_user.email = 'john@mail.com'
+      new_user.password = 'test'
+      new_user.password_confirmation = 'test'
+      expect(new_user).to_not be_valid
+    end
+
+    it 'Should error if a password is not entered' do
+      new_user = User.new
+      new_user.first_name = 'john'
+      new_user.last_name = 'doe'
+      new_user.email = 'john@mail.com'
+      new_user.password = nil
+      expect(new_user).to_not be_valid
+    end
+
+    it 'Should error if password confirmation does not match' do
+      new_user = User.new
+      new_user.first_name = 'john'
+      new_user.last_name = 'doe'
+      new_user.email = 'john@mail.com'
+      new_user.password = 'test'
+      new_user.password_confirmation = 'test1'
+      expect(new_user).to_not be_valid
+    end
+
+    it 'Should be valid if password confirmation does match' do
+      new_user = User.new
+      new_user.first_name = 'john'
+      new_user.last_name = 'doe'
+      new_user.email = 'john@mail.com'
+      new_user.password = 'test'
+      new_user.password_confirmation = 'test'
+      expect(new_user).to be_valid
+    end
+  end
+end
